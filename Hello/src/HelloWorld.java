c5f6a6b81a98fd74666dfc98ebf4d6be648855d8
xutian <xutian0521@qq.com>
2020-11-10 17:54:49 +0800

    数组,string, for循环等

--- Hello/src/HelloWorld.java
+++ Hello/src/HelloWorld.java
@@ -8,7 +8,7 @@ public class HelloWorld {
         System.out.println(c.SayHello());
 
         //线程睡眠
-        Thread.sleep(1000 *3);
+        //Thread.sleep(1000 *3);
         System.out.println("------------------时间格式化------------------------");
         Date dNow = new Date( );
         SimpleDateFormat ft = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss");
@@ -49,5 +49,36 @@ public class HelloWorld {
             System.out.println("name:" + k +"  age:" + v);
         });
 
+        //-------------------数组初始化(和C#一样)-----------------------
+        int[] arr1 = new int[]{ 1, 2, 3 };
+        int[] arr2 = {6, 7, 8};
+        System.out.println("-------------------数组for循环(和C#一样)-----------------------");
+        for (int i=0; i < arr1.length; i++){
+            System.out.println(arr1[i]);
+        }
+
+        System.out.println("-------------------String-----------------------");
+        String s1 = new String();
+        System.out.println(s1);
+
+        char[] chs ={'a','b','c'};
+        String s2 = new String(chs);
+        System.out.println(s2);
+
+        byte[] bys= {97,98,99};
+        String s3 = new String(bys);
+        System.out.println(s3);
+
+        String s4="abc";
+        String s5="abc";
+        System.out.println(s4);
+
+        System.out.println( s4 == s5 );
+        System.out.println(s4.equals(s5));
+
+        System.out.println("-------------------循环字符串--------------------");
+        for (int i =0; i < s5.length(); i++){
+            System.out.println(s5.charAt(i));
+        }
     }
 }
